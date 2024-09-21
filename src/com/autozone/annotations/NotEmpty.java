c474e7136b2f518934323107aa4c1b0c0f81d5dd
Kevin Varela <kevinvarelatalamantes@gmail.com>
2024-09-20 21:01:04 -0600

    Update NotEmpty.java

--- src/com/autozone/annotations/NotEmpty.java
+++ src/com/autozone/annotations/NotEmpty.java
@@ -1,8 +1,6 @@
 package com.autozone.annotations;
-
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
